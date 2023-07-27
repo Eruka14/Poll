5500763ba9216a8a47b030a7a3c91862c0893b3b
Fyodor <shobshob481@gmail.com>
2023-07-27 17:07:52 +0800

    Developed v0.2.0

--- first/src/App.js
+++ first/src/App.js
@@ -3,7 +3,8 @@ import CheckboxList from './components/Poll';
 import './App.css';
 function App() {
   return (
-    <div>  <h1 className="text-2xl font-bold p-4 m-2 mb-4">Асуултын хэсэг:</h1>
+    <div>  
+      <h1 className="text-2xl font-bold p-4 bg-slate-800 text-white">Асуултын хэсэг:</h1>
     <div className="min-h-screen bg-gray-100 flex justify-center items-top">
       <CheckboxList />
     </div>
